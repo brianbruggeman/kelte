5736bd7e1e6a5aae872749f430f853427569b5b0
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2018-06-30 17:01:39 -0500

    Updates movement and visual
        - Character is now rendered in yellow
        - map floor is now grey

--- kelte/api.py
+++ kelte/api.py
@@ -29,61 +29,65 @@ def initialize(debug=None, verbose=None):
 
 
 def convert_event(event):
-    shift_mod = KeyboardModifiers()
-    shift_mod.shift = True
-    # wasd_movement_mapper = {
-    #     # Cardinal
-    #     KeyboardEvent('a'): Direction.LEFT,
-    #     KeyboardEvent('x'): Direction.DOWN,
-    #     KeyboardEvent('w'): Direction.UP,
-    #     KeyboardEvent('d'): Direction.RIGHT,
-    #
-    #     # Diagonals
-    #     KeyboardEvent('q'): Direction.UP_LEFT,
-    #     KeyboardEvent('e'): Direction.UP_RIGHT,
-    #     KeyboardEvent('z'): Direction.DOWN_LEFT,
-    #     KeyboardEvent('c'): Direction.DOWN_RIGHT,
-    #     }
+    """Converts ui.event into ecs.event
 
-    vim_movement_mapper = {
-        # Cardinal
-        KeyboardEvent('h'): Direction.LEFT,
-        KeyboardEvent('j'): Direction.DOWN,
-        KeyboardEvent('k'): Direction.UP,
-        KeyboardEvent('l'): Direction.RIGHT,
+    This maps keyboard entries into something that the system can handle
 
-        # Diagonals
-        KeyboardEvent('y'): Direction.UP_LEFT,
-        KeyboardEvent('u'): Direction.UP_RIGHT,
-        KeyboardEvent('b'): Direction.DOWN_LEFT,
-        KeyboardEvent('n'): Direction.DOWN_RIGHT,
-        }
+    TODO: Add a movement system
+    """
+    if isinstance(event, KeyboardEvent):
+        # wasd_movement_mapper = {
+        #     # Cardinal
+        #     KeyboardEvent('a'): Direction.LEFT,
+        #     KeyboardEvent('x'): Direction.DOWN,
+        #     KeyboardEvent('w'): Direction.UP,
+        #     KeyboardEvent('d'): Direction.RIGHT,
+        #
+        #     # Diagonals
+        #     KeyboardEvent('q'): Direction.UP_LEFT,
+        #     KeyboardEvent('e'): Direction.UP_RIGHT,
+        #     KeyboardEvent('z'): Direction.DOWN_LEFT,
+        #     KeyboardEvent('c'): Direction.DOWN_RIGHT,
+        #     }
 
-    movement = vim_movement_mapper.get(event)  # or wasd_movement_mapper.get(event)
-    if movement:
-        event = Event('MOVE', settings.player, movement)
-        print(event)
+        vim_movement_mapper = {
+            # Cardinal
+            KeyboardEvent('h'): Direction.LEFT,
+            KeyboardEvent('j'): Direction.DOWN,
+            KeyboardEvent('k'): Direction.UP,
+            KeyboardEvent('l'): Direction.RIGHT,
 
-    if event == KeyboardEvent('escape'):
-        exit(0)
+            # Diagonals
+            KeyboardEvent('y'): Direction.UP_LEFT,
+            KeyboardEvent('u'): Direction.UP_RIGHT,
+            KeyboardEvent('b'): Direction.DOWN_LEFT,
+            KeyboardEvent('n'): Direction.DOWN_RIGHT,
+            }
 
-    return event
+        movement = vim_movement_mapper.get(event)  # or wasd_movement_mapper.get(event)
+        if movement:
+            event = Event('MOVE', settings.player, movement)
+            return event
+
+        if event == KeyboardEvent('escape'):
+            exit(0)
 
 
 def cycle():
     done = False
-    tdl.console_set_default_foreground(settings.main_console, tdl.white)
 
     for event in get_events(released=False, mouse_motion=False):
         event = convert_event(event)
-        if event.type == 'MOVE':
+        if event and event.type == 'MOVE':
             entity = event.target
             tile = settings.player.tile.data
             old_pos = entity.position
             new_pos = old_pos + event.data
-            tdl.console_put_char(settings.main_console, old_pos.x, old_pos.y, ' ', tdl.BKGND_NONE)
-            tdl.console_put_char(settings.main_console, new_pos.x, new_pos.y, tile, tdl.BKGND_NONE)
 
+            tdl.console_set_default_foreground(settings.main_console, tdl.grey)
+            tdl.console_put_char(settings.main_console, old_pos.x, old_pos.y, '.', tdl.BKGND_NONE)
+            tdl.console_set_default_foreground(settings.main_console, tdl.yellow)
+            tdl.console_put_char(settings.main_console, new_pos.x, new_pos.y, tile, tdl.BKGND_NONE)
 
     tdl.console_flush()
     return done
@@ -93,7 +97,14 @@ def main(debug=None, verbose=None):
     """Main game loop"""
     verbose = True if debug else max(int(verbose or 0), 0)  # cap minimum at 0
 
+    # fill map
+    tdl.console_set_default_foreground(settings.main_console, tdl.grey)
+    for y in range(settings.height):
+        for x in range(settings.width):
+            tdl.console_put_char(settings.main_console, x, y, '.', tdl.BKGND_NONE)
+
     pos = settings.player.position
+    tdl.console_set_default_foreground(settings.main_console, tdl.yellow)
     tdl.console_put_char(settings.main_console, pos.x, pos.y, '@', tdl.BKGND_NONE)
     while not tdl.console_is_window_closed():
         if cycle():
